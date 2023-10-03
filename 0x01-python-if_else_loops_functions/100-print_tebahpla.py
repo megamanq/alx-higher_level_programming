2413534cb494d26dff7dc9206001ef6ec60ed778
kamel <kamelabouchaala@gmail.com>
2023-10-03 14:49:55 -0700

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for x in range(122, 96, -1):
-    if x % 2 != 0:
-        print("{}".format(chr(x - 32)), end="")
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{:c}".format(i), end="")
     else:
-        print("{}".format(chr(x)), end="")
+        print("{:c}".format(i - 32), end="")
