d5698e49af878eba62f7c76c30dd141e7f9d4af3
kamel <kamelabouchaala@gmail.com>
2023-10-07 14:52:24 -0700

    fix  4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,7 @@ import hidden_4
 def main():
     lng = dir(hidden_4)
     for i in range(len(lng)):
-        if(lng[i][0] != '_'):
+        if (lng[i][0] != '_'):
             print("{}".format(lng[i]))
 
 
