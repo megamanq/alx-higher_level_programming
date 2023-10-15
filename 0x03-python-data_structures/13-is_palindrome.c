7af8bb4eb15866dadadf32384404b12867797b62
kamel <kamelabouchaala@gmail.com>
2023-10-15 14:51:09 -0700

    fix big list - 13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *curr;
 	int i = 0, len = 0;
-	int arr[1024];
+	int arr[5000];
 
 	if (*head == NULL)
 	{
--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -3,33 +3,27 @@
 #include "lists.h"
 
 /**
- * main - check the code for
+ * main - create long even numbered list palindrome and check
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    listint_t *head;
+	listint_t *head;
+	int i;
 
-    head = NULL;
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 1);
-    print_listint(head);
+	head = NULL;
+	for (i = 0; i < 1001; i++)
+		add_nodeint_end(&head, i);
+	for (i = 1000; i >= 0; i--)
+		add_nodeint_end(&head, i);
 
-    if (is_palindrome(&head) == 1)
-        printf("Linked list is a palindrome\n");
-    else
-        printf("Linked list is not a palindrome\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
 
-    free_listint(head);
+	free_listint(head);
 
-    return (0);
+	return (0);
 }
