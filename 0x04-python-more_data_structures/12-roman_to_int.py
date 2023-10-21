776bd2d6db9e5235b0a359abc46568b79b73e0c4
kamel <kamelabouchaala@gmail.com>
2023-10-21 13:44:07 -0700

    add 12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    dict = {"I" : 1, "V" : 5, "X" : 10, "L" : 50,
-            "C" : 100, "D" : 500, "M" : 1000}
-    res = 0
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    result = 0
+    prev_value = 0
 
-    if type(roman_string) != str or not roman_string:
-        return 0
-    else:
-        for char in roman_string:
-            for key in dict.keys():
-                if char == key:
-                    res = res + dict[key]
-        return res
+    for numeral in reversed(roman_string):
+        value = roman_values.get(numeral, 0)
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+
+    return result
