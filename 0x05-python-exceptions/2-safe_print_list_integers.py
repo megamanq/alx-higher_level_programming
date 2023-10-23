f5990d2556a476b3cc07c094060825c09d8a3aab
kamel <kamelabouchaala@gmail.com>
2023-10-23 12:52:45 -0700

    fix 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,11 +9,9 @@ def safe_print_list_integers(my_list=[], x=0):
     nb = 0
     for y in range(0, x):
         try:
-            if y >= len(my_list):
-                raise ValueError("x is greater than the length of my_list")
             print("{:d}".format(my_list[y]), end="")
             nb += 1
-        except (TypeError, ValueError):
+        except(TypeError, ValueError):
             pass
     print()
-    return nb
+    return(nb)
