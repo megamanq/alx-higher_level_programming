6025f72fa2b8e6454332074e05de4a35fbee21d5
kamel <kamelabouchaala@gmail.com>
2023-10-23 12:29:07 -0700

    fix 00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@
 
     """
 
+
 def safe_print_list(my_list=[], x=0):
     nb = 0
     for i in range(0, x):
@@ -14,4 +15,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return nb
-
